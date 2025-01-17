c118ee0410542ba10b831959f58c8c7bac9aa52a
Paule Macedo <paulemacedo@outlook.com>
2025-01-17 03:18:26 -0300

    Add new programs for calculating days, averages, password generation, file copying, character counting, and line conversion; include .gitignore

---
+++ 2 - Escrutura de Dados/1 - Sorting algorithms/Bubble.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+/*
+    O primeiro elemento é comparado com cada elemento no array. É feita a troca
+entre os elementos quando a ordem buscada (crescente ou decrescente) é 
+encontrada. 
+
+Após o primeiro elemento ser comparado com todos os outros e sendo realizada as 
+trocas necessárias, então, na primeira posição estará armazenado o valor 
+pretendido (menor, se for crescente ou maior, se for decrescente). 
+
+Faz-se o mesmo processo com o segundo elemento, depois com o terceiro e assim
+sucessivamente até que o array esteja corretamente ordenado
+
+*/
+
+void bubblesort (int A[], int n)
+{
+    for (int i = 0; i < n-1; i++)
+    {
+        for (int j = i+1; j < n; j++)
+        {
+            if (A[i] > A[j])
+            {
+                int aux = A[i];
+                A[i] = A[j];
+                A[j] = aux;
+            }
+            
+        }
+        
+    }
+
+}
+
+int main ()
+{
+int list[10] = {10,2,5,1,66,34,6,4,3,5};
+int size = sizeof(list) / sizeof(list[0]);
+
+bubblesort(list, size);
+
+for (int i = 0; i < 10; i++) {
+    cout << list[i] << " ";
+}
+}
